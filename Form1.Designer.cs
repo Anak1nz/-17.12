9b79c9292534f3d01b43074e3ce390b00161d7d9
Vova <108748554+Mrkurort@users.noreply.github.com>
2024-12-17 11:28:36 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Работа_17._12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtStep = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(57, 400);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(100, 20);
+            this.txtX.TabIndex = 2;
+            // 
+            // txtStep
+            // 
+            this.txtStep.Location = new System.Drawing.Point(313, 400);
+            this.txtStep.Name = "txtStep";
+            this.txtStep.Size = new System.Drawing.Size(100, 20);
+            this.txtStep.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(433, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "расчет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 368);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(350, 368);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Шаг";
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(183, 400);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(100, 20);
+            this.txtY.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(223, 368);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Y";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(57, 61);
+            this.chart1.Name = "chart1";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "На какой высоте живут бананы";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(712, 300);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Работа_17._12.Properties.Resources.Снимок_экрана_2024_12_17_112102;
+            this.pictureBox1.Location = new System.Drawing.Point(524, 373);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(245, 50);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtStep);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Построения графика";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtStep;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Работа_17._12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtX.Text))
+            {
+                MessageBox.Show("Введите корректное число для X");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtY.Text))
+            {
+                MessageBox.Show("Введите корректное число для Y");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtStep.Text))
+            {
+                MessageBox.Show("Введите корректное число шага");
+                return;
+            }
+            double X = double.Parse(txtX.Text);
+            if (!double.TryParse(txtX.Text, out X))
+            { 
+                MessageBox.Show("Введите корректное число для X");
+            return;
+            }
+            double Y = double.Parse(txtY.Text);
+            if (!double.TryParse(txtY.Text, out Y))
+            {
+                MessageBox.Show("Введите корректное число для Y");
+                return;
+            }
+            double Step = double.Parse(txtStep.Text);
+            if (!double.TryParse(txtStep.Text, out Step) || Step <= 0)
+            {
+                MessageBox.Show("Введите корректное число шага");
+                return;
+            }
+            if (!Proverka(Y))
+            {
+                return;
+            }
+
+            chart1.Series.Clear();
+            var series = new Series
+            {
+                ChartType = SeriesChartType.Line,
+            };
+            chart1.Series.Add(series);
+            string formula = $"y = 2^x * sqrt(x) + |X|^(1/3) * sqrt(exp(x-1)) / sin(Y)";
+            series.LegendText = formula;
+
+            for (double x = 0.1; x <= 10; x += Step)
+            {
+                double a = Math.Pow(2, x) * Math.Sqrt(x) + Math.Pow(Math.Abs(X), 1.0 / 3.0) * Math.Sqrt(Math.Exp(x - 1)) / Math.Sin(Y);
+                series.Points.AddXY(x, a);
+            }
+        }
+
+        private bool Proverka(double y)
+        {
+
+            if (Math.Sin(y) == 0)
+            {
+                MessageBox.Show("Синус Y равен 0, что вызовет деление на ноль");
+                return false;
+            }
+            return true;
+        }
+    }
+}
